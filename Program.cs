34f22eacdf7f7754fe2aa36b2c4d419bd5275f32
Gio Oliveira <giothais.silva@gmail.com>
2025-05-21 18:30:59 -0300

    fix: cors

--- Program.cs
+++ Program.cs
@@ -6,6 +6,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Adicione a política de CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MinhaPoliticaCors", policy =>
+    {
+        policy.WithOrigins("*") // origem permitida
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -14,6 +26,8 @@ var connectionString = builder.Configuration.GetConnectionString("AppDbConnectio
 builder.Services.AddDbContext<AppDbContext>(options => options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 var app = builder.Build();
+
+app.UseCors("MinhaPoliticaCors");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
